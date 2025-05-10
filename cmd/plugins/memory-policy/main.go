0c634311c687d8c25c9cd62193ba8c5cf6c98ea4
Antti Kervinen <antti.kervinen@intel.com>
2025-05-10 21:42:06 +0300

    memory-policy: rudimentary annotation support

--- cmd/plugins/memory-policy/main.go
+++ cmd/plugins/memory-policy/main.go
@@ -19,6 +19,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	_ "sigs.k8s.io/yaml"
 
@@ -26,6 +27,9 @@ import (
 
 	"github.com/containerd/nri/pkg/api"
 	"github.com/containerd/nri/pkg/stub"
+
+	libmem "github.com/containers/nri-plugins/pkg/resmgr/lib/memory"
+	system "github.com/containers/nri-plugins/pkg/sysfs"
 )
 
 type plugin struct {
@@ -41,6 +45,7 @@ const (
 )
 
 var (
+	sys system.System
 	log *logrus.Logger
 )
 
@@ -75,15 +80,121 @@ func pprintCtr(pod *api.PodSandbox, ctr *api.Container) string {
 	return fmt.Sprintf("%s/%s:%s", pod.GetNamespace(), pod.GetName(), ctr.GetName())
 }
 
+// associate adds new key-value pair to a map, or updates existing
+// pair if called with override. Returns true if added/updated.
+func associate(m map[string]string, key, value string, override bool) bool {
+	if _, exists := m[key]; override || !exists {
+		m[key] = value
+		return true
+	}
+	return false
+}
+
+// effectiveAnnotations returns map of annotation key prefixes and
+// values that are effective for a container.
+// Example: a container-specific pod annotation
+//
+//	mode.memory-policy.nri.io/CTRNAME: MPOL_INTERLEAVE
+//	nodes.memory-policy.nri.io/CTRNAME: same-package
+//	flags.memory-policy.nri.io/CTRNAME: MPOL_F_STATIC_NODES
+//
+// shows up as
+//
+//	effAnn["mode"] = "MPOL_INTERLEAVE"
+//	effAnn["nodes"] = "same-package"
+//	effAnn["flags"] = "MPOL_F_STATIC_NODES"
+func effectiveAnnotations(pod *api.PodSandbox, ctr *api.Container) map[string]string {
+	effAnn := map[string]string{}
+	for key, value := range pod.GetAnnotations() {
+		annPrefix, hasSuffix := strings.CutSuffix(key, annotationSuffix+"/"+ctr.Name)
+		if hasSuffix {
+			// Override possibly already found pod-level annotation.
+			log.Tracef("- found container-specific annotation %q", key)
+			associate(effAnn, annPrefix, value, true)
+			effAnn[annPrefix] = value
+			continue
+		}
+		annPrefix, hasSuffix = strings.CutSuffix(key, annotationSuffix)
+		if hasSuffix {
+			// Do not override if there already is a
+			// container-level annotation.
+			if associate(effAnn, annPrefix, value, false) {
+				log.Tracef("- found pod-level annotation %q", key)
+			} else {
+				log.Tracef("- ignoring pod-level annotation %q due to a container-level annotation", key)
+			}
+			continue
+		}
+		log.Tracef("- ignoring annotation %q", key)
+	}
+	return effAnn
+}
+
 // CreateContainer modifies container when it is being created.
 func (p *plugin) CreateContainer(ctx context.Context, pod *api.PodSandbox, ctr *api.Container) (*api.ContainerAdjustment, []*api.ContainerUpdate, error) {
+	annMode := ""
+	annNodes := ""
+	annFlags := ""
 	ppName := pprintCtr(pod, ctr)
 	log.Tracef("CreateContainer %s", ppName)
+	for annPrefix, value := range effectiveAnnotations(pod, ctr) {
+		switch {
+		case annPrefix == "mode":
+			annMode = value
+		case annPrefix == "nodes":
+			annNodes = value
+		case annPrefix == "flags":
+			annFlags = value
+		}
+	}
+	if annMode == "" {
+		log.Tracef("no memory policy mode specified for %s", ppName)
+		return nil, nil, nil
+	}
+
+	mode, ok := api.MpolMode_value[annMode]
+	if !ok {
+		log.Errorf("invalid memory policy mode %q for %s", annMode, ppName)
+		return nil, nil, fmt.Errorf("invalid memory policy mode %q", annMode)
+	}
+
+	flags := []api.MpolFlag{}
+	if annFlags != "" {
+		for _, annFlag := range strings.Split(annFlags, ",") {
+			if annFlag == "" {
+				continue
+			}
+			annFlag = strings.TrimSpace(annFlag)
+			if flag, ok := api.MpolFlag_value[annFlag]; ok {
+				flags = append(flags, api.MpolFlag(flag))
+			} else {
+				log.Errorf("invalid memory policy flag %q for %s", annFlag, ppName)
+				return nil, nil, fmt.Errorf("invalid memory policy flag %q", annFlag)
+			}
+		}
+	}
+
+	nodes := ""
+	if annNodes != "" {
+		nodeIds := sys.NodeIDs()
+		ctrCpus := ctr.GetLinux().GetResources().GetCpu().GetCpus()
+		switch {
+		case annNodes == "same-package":
+			// Use all nodes from the same package as the container's CPUs
+			if ctrCpus != "" {
+				log.Errorf("NOT IMPLEMENTED: get nodes for packages of CPUs %q", ctrCpus)
+				return nil, nil, fmt.Errorf("NOT IMPLEMENTED: nodes:same-package for %s", ppName)
+			}
+		case annNodes == "same-nodes":
+			if ctrCpus != "" {
+				log.Errorf("NOT IMPLEMENTED: get nodes for CPUs %q", ctrCpus)
+				return nil, nil, fmt.Errorf("NOT IMPLEMENTED: nodes:same-nodes for %s", ppName)
+			}
+		}
+		nodes = libmem.NewNodeMask(nodeIds...).MemsetString()
+	}
 	ca := api.ContainerAdjustment{}
-	ca.SetLinuxMemoryPolicy(
-		api.MpolMode_MPOL_INTERLEAVE,
-		"0-1",
-		api.MpolFlag_MPOL_F_STATIC_NODES)
+	ca.SetLinuxMemoryPolicy(api.MpolMode(mode), nodes, flags...)
 	log.Debugf("CreateContainer %s: adjust: %+v", ppName, ca)
 	return &ca, nil, nil
 }
@@ -128,6 +239,11 @@ func main() {
 		if err = p.setConfig(config); err != nil {
 			log.Fatalf("error applying configuration from file %q: %s", configFile, err)
 		}
+	}
+
+	sys, err = system.DiscoverSystem(system.DiscoverCPUTopology)
+	if err != nil {
+		log.Fatalf("failed to discover CPU topology needed by -dist: %v", err)
 	}
 
 	opts := []stub.Option{
