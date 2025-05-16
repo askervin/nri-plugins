510cbb35a4948a4499add2e1c43bc021cea6f599
Antti Kervinen <antti.kervinen@intel.com>
2025-05-16 14:44:30 +0300

    e2e: first test with successful helm-installed nri-memory-policy
    Signed-off-by: Antti Kervinen <antti.kervinen@intel.com>

--- test/e2e/memory.test-suite/memory-policy/n6-hbm-cxl/test01-policies/code.var.sh
+++ test/e2e/memory.test-suite/memory-policy/n6-hbm-cxl/test01-policies/code.var.sh
@@ -1,1 +1,12 @@
+helm-terminate
 helm_config=$(instantiate helm-config.yaml) helm-launch memory-policy
+
+ANN0="class.memory-policy.nri.io: interleave-all" \
+ANN1="policy.memory-policy.nri.io/container.pod0c1: |+
+      mode: MPOL_BIND
+      nodes: 4,5
+      flags:
+      - MPOL_F_STATIC_NODES" \
+ANN2="class.memory-policy.nri.io/container.pod0c2: interleave-cpu-nodes" \
+CONTCOUNT=3 \
+create besteffort
